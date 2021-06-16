cc8774aff1e5ca97aa8cdadc0eaace079c0f1e71
pavana sri <paripalepu@gmail.com>
2021-06-16 10:38:54 +0530

    firstcommit

--- server/models/posts.js
+++ server/models/posts.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const Schema = mongoose.Schema;
 const postSchema = mongoose.Schema({
     category: { type: Schema.Types.ObjectId,ref:"Category"},
     title: String,
